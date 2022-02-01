2794b11ec21bde754e0680399d73f65be1d5c770
John Trampe <73605029+jtrampe77@users.noreply.github.com>
2022-02-01 11:47:49 -0700

    globalstyles

--- react-starter-kit/src/App.js
+++ react-starter-kit/src/App.js
@@ -1,9 +1,16 @@
+// Function ===> Component JSX(markup/data)
+// Components MUST START FROM A SINGLE NODE - element
+// Wrap them in a single element - <div> or <> this is a react fragment
+
 
 function App() {
   return (
+    <>
     <header>
       <h1>React Starter Kit</h1>
     </header>
+    </>
+   
   );
 }
 
