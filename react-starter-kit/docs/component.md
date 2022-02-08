0e1c432918301ea74e24668476c0df61f704edb7
John Trampe <73605029+jtrampe77@users.noreply.github.com>
2022-02-08 10:03:01 -0700

    buttons added

---
+++ react-starter-kit/docs/component.md
@@ -0,0 +1,12 @@
+# Component Basics
+
+## Resuable Markup/Style
+- data aware
+
+1. Create Components
+1. Naming Best Practices
+1. Closed / Open Component
+
+
+Naming Best Practices
+1. Component Uppercase Upper CamelCase
--- react-starter-kit/src/App.js
+++ react-starter-kit/src/App.js
@@ -2,15 +2,21 @@
 // Components MUST START FROM A SINGLE NODE - element
 // Wrap them in a single element - <div> or <> this is a react fragment
 
+import Button from "./components/button/Button";
 
 function App() {
   return (
     <>
     <header>
       <h1>React Starter Kit</h1>
+      <Button title="Default Button" type="default"/>
+      <Button title="Warning Button" type="warning"/>
+      <Button title="Danger Button" type="danger"/>
+      <Button title="Small Button" type="small"/>
+      <Button title="Small Warning Button" type="smallWarning"/>
     </header>
     </>
-   
+    
   );
 } 
 
