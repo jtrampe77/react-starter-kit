bf1cdff1c0e1064949a595a91615a189192279c9
John Trampe <73605029+jtrampe77@users.noreply.github.com>
2022-03-08 11:02:03 -0700

    update

--- react-protected-authentication/src/pages/dashboard/DashBoardPage.js
+++ react-protected-authentication/src/pages/dashboard/DashBoardPage.js
@@ -1,28 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {AppBar} from './../../components/appbar'
 import {Link} from 'react-router-dom'
+import { onAuthStateChanged } from 'firebase/auth'
+import {auth} from 'libs/firebase'
+import {useNavigate} from 'react-router-dom'
 
 function DashBoardPage(props) {
-  return (
+
+  const [isUser, setIsUser] = useState(false)
+  const navigator = useNavigate()
+
+  onAuthStateChanged(auth, (user)=>{
+    if(user){
+        setIsUser(true)
+    }else{
+        setIsUser(false)
+        //login page
+        navigator('/')
+    }
+  })
+
+  if(isUser){
+    return(
+      <>
+      <nav>
+        <ul>
+          <li>
+          <Link to="/">Login</Link>
+          </li>
+          
+          <li>
+          <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+      </nav>
   
-    <>
-    <nav>
-      <ul>
-        <li>
-        <Link to="/">Login</Link>
-        </li>
+      <AppBar />
+      </>
         
-        <li>
-        <Link to="/dashboard">Dashboard</Link>
-        </li>
-      </ul>
-    </nav>
+    )
+  
+  }else{
+    return null
+  }
 
-    <AppBar />
-    </>
-      
-
-  )
 }
 
 export default DashBoardPage
